d19506ad88a00294f25bd6744417f88633792d89
Jensen Turner <jenst18@lakesideschool.org>
2017-07-25 10:18:27 -0700

    abstract out parsing

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-
+import parse_housing
 
 ##these functions initialize weights and biases with non-zero values
 def weight_variable(shape):
@@ -17,32 +17,21 @@ def bias_variable(shape):
 learning_rate = .05
 num_epochs = 200
 
-#list of training files
-filename_queue = tf.train.string_input_producer(["data/5features.csv"])
-#read files
-reader = tf.TextLineReader()
-key, value = reader.read(filename_queue)
-
-#default values and type of input
-record_defaults = [[9000], [1989], [3], [6], [2007]]
-
-#read columns
-col1, col2, col3, col4, col5 = tf.decode_csv(value, record_defaults=record_defaults)
-features = tf.stack([col1, col2, col3, col4])
+features = parse_housing.parse()
 
 #### MODEL
 
-# this is the placeholder for the inputs. takes an arbitrary size of training set with 5 features in each example
+# this is the placeholder for the input layer. takes an arbitrary size of training set with 5 features in each example
 x = tf.placeholder(tf.float32, (None, 5))
 
-## weights and biases for the first layer. first layer has 10 neurons
+## weights and biases for the first hidden layer - 10 neurons
 W1 = weight_variable([5, 10])
 b1 = bias_variable([10])
 
-##output of first layer
+## output of first hidden layer
 a1 = tf.sigmoid(tf.matmul(W1, x) + b1)
 
-## weights and biases for second/output layer. has one output
+## weights and biases for output layer - 1 neuron
 W2 = weight_variable([10, 1])
 b2 = bias_variable([1])
 
