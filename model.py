3fd5463c9be6a7db45464768d31359de97def4a4
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-07-25 18:20:41 -0700

    Added 3 new features
    1st floor sq ft
    2nd floor sq ft
    overall quality

--- model.py
+++ model.py
@@ -36,27 +36,27 @@ reader = tf.TextLineReader()
 key, value = reader.read(filename_queue)
 
 #default values and type of input
-record_defaults = [[9000], [1989], [3], [6], [2007], [150000]]
+record_defaults = [[9000], [1989], [3], [6], [2007], [0], [0], [0], [150000]]
 
 #read columns
-col1, col2, col3, col4, col5, col6= tf.decode_csv(value, record_defaults=record_defaults)
-features = tf.stack([col1, col2, col3, col4, col5])
+col1, col2, col3, col4, col5, col6, col7, col8, col9= tf.decode_csv(value, record_defaults=record_defaults)
+features = tf.stack([col1, col2, col3, col4, col5, col6, col7, col8])
 
 
 #### MODEL
 
 # this is the placeholder for the input layer. takes an arbitrary size of training set with 5 features in each example
-x = tf.placeholder(tf.float32, (None, 5))
+x = tf.placeholder(tf.float32, (None, 8))
 
 ## weights and biases for the first hidden layer - 10 neurons
-W1 = xavier_init([5, 10])
-b1 = bias_variable([10])
+W1 = xavier_init([8, 8])
+b1 = bias_variable([8])
 
 ## output of first hidden layer
 a1 = tf.sigmoid(tf.matmul(x, W1) + b1)
 
 ## weights and biases for second hidden layer - 20 neuron
-W2 = xavier_init([10, 10])
+W2 = xavier_init([8, 10])
 b2 = bias_variable([10])
 
 #output of second hidden layer
@@ -84,7 +84,7 @@ y_ = tf.placeholder(tf.float32)
 rmslerror = rmsle(y, y_, num_training_inputs)
 
 # mean squared error
-error = tf.losses.log_loss(y_, y)
+error = tf.losses.mean_squared_error(y_, y)
 
 # gradient descent for learning
 trainstep = tf.train.GradientDescentOptimizer(learning_rate).minimize(error)
@@ -107,14 +107,20 @@ with tf.Session() as sess:
     for i in tqdm(range(num_training_inputs)):
 
         #grab line value
-        feature, expected_output = sess.run([features, col6])
+        feature, expected_output = sess.run([features, col9])
 
-        #add line to inputs/expected outputs
+        #scale features appropriately
+        feature[0] = feature[0]/1000
+        feature[1] = feature[1]/1000
+        feature[4] = feature[4]/1000
+        feature[6] = feature[6]/1000
+        feature[7] = feature[7]/1000
+        # add line to inputs/expected outputs
         inputs.append(feature)
         expected_outputs.append(expected_output/1000000)
     coord.request_stop()
     coord.join(threads)
-    inputs = np.resize(inputs, (num_training_inputs, 5))
+    inputs = np.resize(inputs, (num_training_inputs, 8))
 
     expected_outputs = np.resize(expected_outputs, (num_training_inputs, 1))
 
@@ -124,9 +130,11 @@ with tf.Session() as sess:
             print(sess.run(rmslerror, feed_dict={x: inputs, y_: expected_outputs}))
 
     print(sess.run(rmslerror, feed_dict = {x: inputs, y_: expected_outputs}))
-
+    print(sess.run(y, feed_dict = {x: inputs}))
+    print(inputs)
     #variablesaver
-    saver = tf.train.Saver()
-    save_path=saver.save(sess, "./savedmodels/variableSave")
-    print("Model saved in file: %s" % save_path)
+    if (sess.run(rmslerror, feed_dict = {x: inputs, y_: expected_outputs}) != 'nan'):
+        saver = tf.train.Saver()
+        save_path=saver.save(sess, "./savedmodels/variableSave")
+        print("Model saved in file: %s" % save_path)
 
