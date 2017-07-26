a97f8ab6a6617295f95b58999c1e3e3069087eca
Mark Chen <firemark62@gmail.com>
2017-07-26 09:26:03 -0700

    read file

--- features.py
+++ features.py
@@ -1,16 +1,13 @@
 
 
-# Any results you write to the current directory are saved as output.
 import pandas as pd
-import numpy as np
-import matplotlib as plt
 
-#Import Dataset
-df = pd.read_csv('../data/train.csv')
-df1 = pd.read_csv('../data/test.csv')
+df_train = pd.read_csv('./data/train.csv')
+print (df_train.columns)
 
 
-#Select numerical data types
-set(df.dtypes.tolist())
-dfnum = df.select_dtypes(include = ['float64', 'int64'])
-dfnum.info()
+
+
+
+
+
