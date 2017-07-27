9239fb83c626240f67a8721c104c1e8b02f83f40
Eli Winkelman <eliwinkelman@Elis-MacBook-Pro-2.local>
2017-07-27 12:53:45 -0700

    model2point0

--- model2point0.py
+++ model2point0.py
@@ -16,5 +16,19 @@ training_expected = np.delete(training_expected, 1, axis=1)
 validation_expected = np.delete(validation_expected, 1, axis=1)
 
 
+####Hyperparams
+learning_rate = .05
+epochs = 10000
+shape = [57, 100, 70, 50, 30, 20, 10, 1]
+activate_output = True
+report_interval = 1000
+model_name = "model2point0"
+
+with NeuralNet.new(model_name, shape, optimizer_params=learning_rate, activate_output=True) as NN:
+    NN.learn(training_features, training_expected, epochs, report_interval=report_interval)
+    NN.validate(validation_features, validation_expected)
+
+
+
 
 
