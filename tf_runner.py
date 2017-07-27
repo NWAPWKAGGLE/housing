c24d178ad454b820dcd5d8f87c206ec2f5725252
Jensen Turner <jenst18@lakesideschool.org>
2017-07-27 11:02:47 -0700

    first actual working version of NeuralNet class

--- tf_runner.py
+++ tf_runner.py
@@ -6,6 +6,8 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
+debug = False
+
 
 class NeuralNet:
     @classmethod
@@ -19,8 +21,7 @@ class NeuralNet:
                                  bias_generator or cls.default_bias_generator, activator, activate_output)
 
         err = tf.identity(error_metric(y_, y), name="err")
-        learning_rate = tf.Variable(base_learning_rate, dtype=tf.float32, name="learning_rate")
-        train = optimizer(learning_rate, name="train").minimize(err)
+        train = optimizer(base_learning_rate, name="train").minimize(err)
 
         return cls(model_name)
 
@@ -29,7 +30,8 @@ class NeuralNet:
 
         selector = path.join(save_dir, model_name, '*.ckpt.meta')
         newest = max(iglob(selector), key=path.getctime)
-
+        if debug:
+            print(newest)
         return cls(model_name, newest)
 
     def __init__(self, model_name, restore_file=None):
@@ -43,14 +45,18 @@ class NeuralNet:
         if self.restore_file is not None:
             saver = tf.train.import_meta_graph(self.restore_file)
             saver.restore(sess, self.restore_file[:-5])
+            self.saver = saver
             self.trained = True
-        sess.run(tf.global_variables_initializer())
+        else:
+            self.saver = tf.train.Saver()
+            sess.run(tf.global_variables_initializer())
         self.sess = sess
         self.managed = True
         return self
 
-    def __exit__(self):
+    def __exit__(self, type, value, traceback):
         self.managed = False
+        self.saver = None
         self.sess.close()
 
     @staticmethod
@@ -60,11 +66,12 @@ class NeuralNet:
             up_size = shape[i]
             down_size = shape[i + 1]
 
-            print('on layer with up {0} and down {1}'.format(up_size, down_size))
+            if debug:
+                print('mid-layer [{0}, {1}]'.format(up_size, down_size))
 
             w = tf.Variable(weight_generator([up_size, down_size]))
             b = tf.Variable(bias_generator([down_size]))
-            if (i < len(shape) - 2):  # output
+            if (i == len(shape) - 2):  # output
                 if activate_output:
                     a_set.append(activator(tf.matmul(a_set[-1], w) + b, name='y'))
                 else:
@@ -85,37 +92,34 @@ class NeuralNet:
     def xavier_weight_generator(shape):
         raise NotImplementedError("Xavier generation isn't implemented yet")
 
-    def learn(self, xvals, y_vals, epochs, report_interval=10000, learning_rate=None,
-              save_dir=path.join('', 'model_saves')):
-
+    def learn(self, xvals, y_vals, epochs, report_interval=10000, save_dir=path.join('', 'model_saves')):
         if not self.managed:
             raise RuntimeError("Class TFRunner must be resource-managed by _with_ statement")
-
-        if learning_rate is not None:
-            self.sess.run(tf.assign([v for v in tf.global_variables() if v.op.name == 'learning_rate'][0],
-                                    learning_rate))
-
-        print(self.sess.run('x', feed_dict={'x': xvals}))
-        print(self.sess.run('y_', feed_dict={'y_': y_vals}))
-
-        saver = tf.train.Saver()
+        if debug:
+            print(self.sess.run('x:0', feed_dict={'x:0': xvals}))
+            print(self.sess.run('y_:0', feed_dict={'y_:0': y_vals}))
 
         for i in tqdm(range(epochs)):
-            self.sess.run('train', feed_dict={'x': xvals, 'y_': y_vals})
+            self.sess.run('train', feed_dict={'x:0': xvals, 'y_:0': y_vals})
             if (i % report_interval == 0) or (i + 1 == epochs):
-                measured_error = self.sess.run('err', feed_dict={'x': xvals, 'y_': y_vals})
-
-                print('y(x)')
-                print(self.sess.run('y', feed_dict={'x': xvals}))
-                print("y(x) error")
-                print(measured_error)
-
-                save_path = path.join(save_dir,
-                                      '{0}__{1}.ckpt'.format(measured_error, str(datetime.now()).replace(':', '_')))
-                save_path = saver.save(self.sess, save_path)
-                print("Model saved in file: {0}".format(save_path))
-
+                measured_error = self.sess.run('err:0', feed_dict={'y_:0': y_vals, 'x:0': xvals})
+                save_path = self._save(save_dir, measured_error, i, epochs)
+                if debug:
+                    y = self.sess.run('y:0', feed_dict={'x:0': xvals})
+                    print('y(x)')
+                    print(y)
+                    print('y(x) measured error')
+                    print(measured_error)
+                    print("Model saved in file: {0}".format(save_path))
         self.trained = True
+
+    def _save(self, save_dir, err, i, epochs):
+        if not self.managed:
+            raise RuntimeError("TFRunner must be in with statement")
+        s_path = path.join(save_dir, self.model_name, '{0}__{1}_{2}__{3}.ckpt'.format(err, i, epochs,
+                                                                                      str(datetime.now()).replace(':',
+                                                                                                                  '_')))
+        return self.saver.save(self.sess, s_path)
 
     def validate(self, qvals, q_vals):
         if not self.managed:
@@ -124,25 +128,29 @@ class NeuralNet:
             if not self.trained:
                 raise RuntimeError("This TFRunner has not been trained yet")
             else:
-                print("q")
-                print(self.sess.run('x', feed_dict={'x': qvals}))
-                print("y(q)")
-                print(self.sess.run('y', feed_dict={'x': qvals}))
-                print("q_")
-                print(self.sess.run('y_', feed_dict={'y_': q_vals}))
-                print("y(q) error")
-                print(self.sess.run('err', feed_dict={'x': qvals, 'y_': q_vals}))
+                measured_error = self.sess.run('err:0', feed_dict={'x:0': qvals, 'y_:0': q_vals})
+                if debug:
+                    print("q")
+                    print(self.sess.run('x:0', feed_dict={'x:0': qvals}))
+                    print("y(q)")
+                    print(self.sess.run('y:0', feed_dict={'x:0': qvals}))
+                    print("q_")
+                    print(self.sess.run('y_:0', feed_dict={'y_:0': q_vals}))
+                    print("y(q) error")
+                    print(measured_error)
+                return measured_error
 
-    def run(self, tvals):
+    def feed_forward(self, tvals):
         if not self.managed:
             raise RuntimeError("TFRunner must be in with statement")
         else:
             if not self.trained:
                 raise RuntimeError("This TFRunner has not been trained yet")
             else:
-                result = self.sess.run('y', feed_dict={'x': tvals})
-                print("t")
-                print(self.sess.run('x', feed_dict={'x': tvals}))
-                print("y(t)")
-                print(result)
+                result = self.sess.run('y:0', feed_dict={'x:0': tvals})
+                if debug:
+                    print("t")
+                    print(self.sess.run('x:0', feed_dict={'x:0': tvals}))
+                    print("y(t)")
+                    print(result)
                 return result
